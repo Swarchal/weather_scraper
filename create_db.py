9ce07be97b46277d798f79d943fb7cc3edcd2be8
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-03 14:38:52 +0000

    remove id column, created automatically

--- create_db.py
+++ create_db.py
@@ -10,7 +10,7 @@ c = con.cursor() # stupid thing
 
 # create table with columns: id, datetime, temp
 c.execute('''
-    CREATE TABLE weather(id INTEGER PRIMARY KEY,
+    CREATE TABLE weather(
     	datetime TEXT,
     	temp REAL,
     	humidity REAL,
