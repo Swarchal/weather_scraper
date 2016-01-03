888737116c8535fe2fc9646679c442779cccedcc
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-03 13:46:23 +0000

    add more measurements

--- bbc_parser.py
+++ bbc_parser.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import urllib2
 from datetime import datetime
 import time
+import re
 
 def create_soup(url):
 	'Create BeautifulSoup object from URL'
@@ -47,3 +48,52 @@ def get_datetime(soup_obj):
 	date_time = datetime.combine(date, time_out)
 	return date_time
 
+
+def get_humidity(soup_obj):
+	'Extract humidity percentage'
+	observations = soup_obj.find_all('', class_= 'observations module')
+	for found in observations:
+		humid_string = str(found.find_all('', class_= "humidity"))
+	humidity_list = [int(s) for s in humid_string if s.isdigit()]
+	humidity = int(''.join(map(str, humidity_list))) # concat list of numbers
+	return humidity
+
+
+def get_windspeed(soup_obj):
+	'Extract wind speed'
+	observations = soup_obj.find_all('', class_= 'observations module')
+	for found in observations:
+		ws_string = str(found.find_all('',
+			class_="units-value windspeed-value windspeed-value-unit-mph"))
+	ws_list = [int(s) for s in ws_string if s.isdigit()]
+	windspeed = int(''.join(map(str, ws_list))) # concat list of numbers
+	return windspeed
+
+
+def get_winddirection(soup_obj):
+	'Extract wind direction'
+	observations = soup_obj.find_all('', class_= 'observations module')
+	for found in observations:
+		dir_string = found.find_all('', class_= 'description blq-hide')
+	direction = str(dir_string)[
+	len('<span class="decription blq-hide">') + 2 :-len('<\\span>') - 1]
+	return direction
+
+
+
+def get_weather(soup_obj):
+	'Extract weather, e.g cloudy or raining'
+	observations = soup_obj.find_all('', class_= 'observations module')
+	for found in observations:
+		weather_str = str(found.find_all('', class_= 'weather-type'))
+	ans = re.findall(r'"(.+?)"', weather_str)[1] # get matches between ""
+	return ans
+
+def get_pressure(soup_obj):
+	'Extract pressure in millibars'
+	observations = soup_obj.find_all('', class_= 'observations module')
+	for found in observations:
+		pressure_string = str(found.find_all('', class_='pressure'))
+	pressure_list = [int(s) for s in pressure_string if s.isdigit()]
+	pressure = int(''.join(map(str, pressure_list))) # concat list of numbers
+	return pressure
